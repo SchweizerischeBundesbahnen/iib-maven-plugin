15e7e95b3a445fd57939b2bba9d74eff21067bd5
u219237 <pascal.moser2@sbb.ch>
2016-01-22 13:07:02 +0100

    Revert "IMP-45 change java doc. delete pom for packing --> problems by debugging. refactoring packing"
    This reverts commit a2dd894f55decd5814473603233bcfe6528e38a6.

--- src/main/java/ch/sbb/maven/plugins/iib/mojos/ValidatePropertiesMojo.java
+++ src/main/java/ch/sbb/maven/plugins/iib/mojos/ValidatePropertiesMojo.java
@@ -8,20 +8,15 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-import ch.sbb.maven.plugins.iib.utils.ProjectType;
-
 /**
- * Goal which reads the propertiey files to figure out if the naming convention for this bar project is observed.
- * It must have the following 3 files:
- * test.parentArtefactId-app or lib
- * inte.parentArtefactId-app or lib
- * prod.parentArtefactId-app or lib
+ * Goal which reads the default.properties file to figure out if the classloader approach for this bar project is consistent. Either all jar nodes in all flows must use a classloader or none of them
+ * should.
  */
 @Mojo(name = "validate-property-name")
 public class ValidatePropertiesMojo extends AbstractMojo {
 
     /**
-     * The path of the properties files to be used from the overrideProperties command.
+     * The name of the default properties file to be generated from the bar file.
      * The default folder is src/main/resources
      */
     @Parameter(property = "iib.configurablePropertiesPath", defaultValue = "${project.build.resources[0].directory}", required = true)
@@ -30,8 +25,8 @@ public class ValidatePropertiesMojo extends AbstractMojo {
     /**
      * The type of the dependency which is addes in the pom.xml of the bar project.
      */
-    @Parameter(property = "iib.projectType", defaultValue = "APPLICATION", required = true)
-    protected ProjectType projectType;
+    @Parameter(property = "iib.projectType", defaultValue = "app", required = true)
+    protected String projectType;
 
     /**
      * The Maven Project Object
@@ -40,9 +35,9 @@ public class ValidatePropertiesMojo extends AbstractMojo {
     protected MavenProject project;
 
     public void execute() throws MojoFailureException {
-        File testProperty = new File(propertyPath + "/test." + project.getParent().getArtifactId() + "-" + projectType.getType() + ".properties");
-        File inteProperty = new File(propertyPath + "/inte." + project.getParent().getArtifactId() + "-" + projectType.getType() + ".properties");
-        File prodProperty = new File(propertyPath + "/prod." + project.getParent().getArtifactId() + "-" + projectType.getType() + ".properties");
+        File testProperty = new File(propertyPath + "/test." + project.getParent().getArtifactId() + "-" + projectType + ".properties");
+        File inteProperty = new File(propertyPath + "/inte." + project.getParent().getArtifactId() + "-" + projectType + ".properties");
+        File prodProperty = new File(propertyPath + "/prod." + project.getParent().getArtifactId() + "-" + projectType + ".properties");
 
         if (!testProperty.isFile() || !inteProperty.isFile() || !prodProperty.isFile()) {
             throw new MojoFailureException("Validation error: The Property files have a wrong name.");
